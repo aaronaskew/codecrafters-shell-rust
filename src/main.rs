2437aed952349bed78680fbfd90279b9e040d7b5
Aaron Askew <aaronaskew@gmail.com>
2026-02-24 07:12:43 -0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     print!("$ ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush()?;
+
+    let mut command = String::new();
+
+    io::stdin().read_line(&mut command)?;
+
+    println!("{}: command not found", command.trim());
+
+    Ok(())
 }
